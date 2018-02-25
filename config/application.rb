e603e52eb4cfc5fe83a7e50fd63ba6b2383a7f06
Alex Koval <al3xander.koval@gmail.com>
2018-02-25 13:18:01 +0300

    Move CORS configuration from application.rb to cors.rb

--- config/application.rb
+++ config/application.rb
@@ -26,15 +26,5 @@ module DTalks
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore, key: '_namespace_key'
     config.middleware.use ActionDispatch::Flash
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-
-        resource '*',
-                 headers: :any,
-                 methods: [:get, :post, :put, :patch, :delete, :options, :head]
-      end
-    end
   end
 end
