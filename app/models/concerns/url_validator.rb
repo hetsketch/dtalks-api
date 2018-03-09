cb838bfbac7dd4499de2d82710dea0f424cda13c
Alex Koval <al3xander.koval@gmail.com>
2018-03-09 09:17:14 +0300

    Change URL regex in url_validator

--- app/models/concerns/url_validator.rb
+++ app/models/concerns/url_validator.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class UrlValidator < ActiveModel::EachValidator
-  URL_REGEX = %r((https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,}))
+  URL_REGEX = /\A(https?:\/\/){1}([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\z/
 
+  #TODO: add i18n message instead hardcoded
   def validate_each(record, attribute, value)
     if value.is_a?(Array)
       record.errors.add(attribute, 'must start with http:// or https://') if value.any? { |e| !e.match?(URL_REGEX) }
