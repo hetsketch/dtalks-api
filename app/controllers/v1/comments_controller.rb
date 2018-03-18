2315ef4d4a3d2515cc3adda8834e36e7f1831e43
Alex Koval <al3xander.koval@gmail.com>
2018-03-18 20:30:57 +0300

    Remove local variable from private method

--- app/controllers/v1/comments_controller.rb
+++ app/controllers/v1/comments_controller.rb
@@ -13,11 +13,12 @@ class V1::CommentsController < ApplicationController
   end
 
   def update
-    comment.update!(comment_params)
+    @comment = comment(params[:id])
+    @comment.update!(comment_params)
   end
 
   def destroy
-    comment&.destroy!
+    comment(params[:id]).destroy!
     render json: { success: true }, status: :ok
   end
 
@@ -32,7 +33,7 @@ class V1::CommentsController < ApplicationController
     params.permit(:text)
   end
 
-  def comment
-    @comment ||= Comment.find(params[:id])
+  def comment(id)
+    Comment.find(id)
   end
 end
