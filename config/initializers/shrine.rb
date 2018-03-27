14b096f545470885c0bf9c4ee6b43dc5ea2651f1
Alex Koval <al3xander.koval@gmail.com>
2018-03-27 20:01:28 +0300

    Change shrine conf

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -13,17 +13,17 @@ elsif Rails.env.test?
     store: Shrine::Storage::Memory.new
   }
 else
-  require 'shrine/storage/s3'
-  s3_options = {
-    access_key_id:     ENV['S3_ACCESS_KEY'],
-    secret_access_key: ENV['S3_ACCESS_SECRET_KEY'],
-    region:            ENV['S3_REGION'],
-    bucket:            ENV['S3_BUCKET']
-  }
-  Shrine.storages = {
-    cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
-    store: Shrine::Storage::S3.new(prefix: 'store', **s3_options)
-  }
+  # require 'shrine/storage/s3'
+  # s3_options = {
+  #   access_key_id:     ENV['S3_ACCESS_KEY'],
+  #   secret_access_key: ENV['S3_ACCESS_SECRET_KEY'],
+  #   region:            ENV['S3_REGION'],
+  #   bucket:            ENV['S3_BUCKET']
+  # }
+  # Shrine.storages = {
+  #   cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
+  #   store: Shrine::Storage::S3.new(prefix: 'store', **s3_options)
+  # }
 end
 
 Shrine.plugin :activerecord
